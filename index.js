b1d5d87119f115b337c791d6a96dc38ab68daba0
Liko <li_xie1992@hotmail.com>
2019-11-27 16:15:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+const map = (array, callback) => {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]));
+    };
+    return newArray;
+}
+
+const reduce = (array, callback, startValue = null) => {
+    let endValue = (!!startValue) ? startValue : array[0]
+    let i = (!!startValue) ? 0 : 1
+    for (; i < array.length; i++) {
+        endValue = callback(array[i], endValue);
+    } 
+    return endValue;
+}
